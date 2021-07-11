408887276098aa3666849889847fa5c0c0249d3d
jingbo <jingbo@niubi.com>
2021-07-11 15:00:53 +0800

    second commit

--- src/main/java/com/example/Web/GitTest.java
+++ src/main/java/com/example/Web/GitTest.java
@@ -3,5 +3,6 @@ package com.example.Web;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("shou dong");
+        System.out.println("shou dong2");
     }
 }
--- src/main/java/com/example/Web/GitTest1.java
+++ src/main/java/com/example/Web/GitTest1.java
@@ -3,5 +3,6 @@ package com.example.Web;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("zidong");
+        System.out.println("zidong2");
     }
 }
