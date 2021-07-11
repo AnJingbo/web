d4f50ad5624242ff2c370d2076faefadaa7836b0
jingbo <jingbo@niubi.com>
2021-07-11 16:42:56 +0800

    push commit

--- src/main/java/com/example/Web/GitTest.java
+++ src/main/java/com/example/Web/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hot-fix1");
         System.out.println("master1");
         System.out.println("hor-fix2");
+        System.out.println("push test");
     }
 }
