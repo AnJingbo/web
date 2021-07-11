38c113ba34b4ce64f4bae63ec0bab6fba864b28c
jingbo <jingbo@niubi.com>
2021-07-11 15:26:18 +0800

    hot-fix commit2

--- src/main/java/com/example/Web/GitTest.java
+++ src/main/java/com/example/Web/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("shou dong");
         System.out.println("shou dong2");
         System.out.println("hot-fix1");
+        System.out.println("hor-fix2");
     }
 }
